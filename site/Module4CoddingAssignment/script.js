99d1a658953feca89e7b92f7e4bc8abef61774e4
Luqman Salad <luqmaansalaat956@gmail.com>
2021-02-02 14:26:03 +0300

    newchange

--- site/Module4CoddingAssignment/script.js
+++ site/Module4CoddingAssignment/script.js
@@ -1,4 +1,7 @@
+
+
 var names=new Array();
+
 names[0]="Yaakov";
 names[1]="John";
 names[2]="Jen";
@@ -11,11 +14,12 @@ names[8]="laura";
 names[9]="jim";
 
 
+
+
 for (var i = 0; i < names.length; i++) {
 	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
         console.log("Goodbye "+ names[i])
-	}
-	else{
+	}else{
 		console.log("Hello "+ names[i])
 	}
 }
